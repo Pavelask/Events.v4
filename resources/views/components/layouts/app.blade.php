284294b544342efd464b5156966c80deded3d5cc
PavelAsk <pavelask@mail.ru>
2024-07-18 08:37:23 +0300

    New site ready

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ config('app.name', 'VIII Всероссийский слёт молодёжи Общественной организации «Всероссийский Электропрофсоюз') }}</title>
+    <title>VIII Всероссийский слёт молодёжи Общественной организации «Всероссийский Электропрофсоюз}</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <link rel="stylesheet" href="{{asset("css/style.css")}}">
