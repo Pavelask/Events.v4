e0f0ea666e7143e030002af53d7667ee4e08a649
PavelAsk <pavelask@mail.ru>
2024-07-18 08:36:29 +0300

    New site ready

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>{{ $title ?? 'Всероссийский Турнир по охране труда и промышленной безопасности в формате интеллектуальной игры «БРЕЙН-РИНГ»' }}</title>
+    <title>{{ config('app.name', 'VIII Всероссийский слёт молодёжи Общественной организации «Всероссийский Электропрофсоюз') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
     <link rel="stylesheet" href="{{asset("css/style.css")}}">
