394b367ede8b10e30981fda5bc7b39ed28cde36f
PavelAsk <pavelask@mail.ru>
2024-07-26 01:22:07 +0300

    Ready Reg Forms

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import laravel, { refreshPaths } from 'laravel-vite-plugin'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 
 export default defineConfig({
     plugins: [
-        // basicSsl({
-        //     /** name of certification */
-        //     name: 'events.v4',
-        //     /** custom trust domains */
-        //     domains: ['events.v4'],
-        //     /** custom certification directory */
-        //     certDir: '/Users/pavelklimov/.config/ssl'
-        // }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: [
