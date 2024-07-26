fb1c127d2ee15f53300d6fcdb58d28e6da98bc4f
PavelAsk <pavelask@mail.ru>
2024-07-26 14:53:04 +0300

    Ready Reg Forms

--- app/Filament/Resources/MembersResource/Pages/ListMembers.php
+++ app/Filament/Resources/MembersResource/Pages/ListMembers.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\MembersResource\Pages;
 
 use App\Filament\Resources\MembersResource;
+use App\Models\Members;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -18,13 +19,17 @@ class ListMembers extends ListRecords
     {
         return [
             'today' => Tab::make('За сегодня')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at', '>=', now()->subDay())),
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('created_at', '>=', now()->subDay()))
+                ->badge(Members::query()->where('created_at', '>=', now()->subDay())->count()),
             'week' => Tab::make('За неделю')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at', '>=', now()->subWeek())),
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('created_at', '>=', now()->subWeek()))
+                ->badge(Members::query()->where('created_at', '>=', now()->subWeek())->count()),
             'months' => Tab::make('За месяц')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at', '>=', now()->subMonths())),
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('created_at', '>=', now()->subMonths()))
+                ->badge(Members::query()->where('created_at', '>=', now()->subMonths())->count()),
             'year' => Tab::make('За год')
-                ->modifyQueryUsing(fn (Builder $query) => $query->where('created_at', '>=', now()->subYear())),
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('created_at', '>=', now()->subYear()))
+                ->badge(Members::query()->where('created_at', '>=', now()->subYear())->count()),
             'all' => Tab::make('Все участники'),
         ];
     }
