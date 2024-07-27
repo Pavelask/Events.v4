6fa790f0b48942d664a2cdfa42a2b6a67108844f
PavelAsk <pavelask@mail.ru>
2024-07-28 01:03:59 +0300

    Ready Reg Forms

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,7 +8,9 @@ use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\View\View;
+use Symfony\Component\HttpFoundation\IpUtils;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -23,14 +25,47 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
+
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        $recaptcha_response = $request->input('g-recaptcha-response');
 
-        $request->session()->regenerate();
+        if (is_null($recaptcha_response)) {
+            return redirect()->back()->with('status', 'Please Complete the Recaptcha to proceed');
+        }
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+
+        $body = [
+            'secret' => config('services.recaptcha.secret'),
+            'response' => $recaptcha_response,
+            'remoteip' => IpUtils::anonymize($request->ip()) //anonymize the ip to be GDPR compliant. Otherwise just pass the default ip address
+        ];
+
+        $response = Http::asForm()->post($url, $body);
+
+        $result = json_decode($response);
+
+        if ($response->successful() && $result->success == true) {
+            $request->authenticate();
+
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        } else {
+            return redirect()->back()->with('status', 'Please Complete the Recaptcha Again to proceed');
+        }
     }
+
+
+//    public function store(LoginRequest $request): RedirectResponse
+//    {
+//        $request->authenticate();
+//
+//        $request->session()->regenerate();
+//
+//        return redirect()->intended(RouteServiceProvider::HOME);
+//    }
 
     /**
      * Destroy an authenticated session.
