36d3ba83884e7222ece045eec9f434e59ae7948f
PavelAsk <pavelask@mail.ru>
2024-09-03 18:24:00 +0300

    Export EXL

--- app/Filament/Resources/MembersResource/Pages/EditMembers.php
+++ app/Filament/Resources/MembersResource/Pages/EditMembers.php
@@ -10,6 +10,11 @@ class EditMembers extends EditRecord
 {
     protected static string $resource = MembersResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
